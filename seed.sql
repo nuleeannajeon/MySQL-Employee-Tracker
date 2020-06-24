80ca0ddebf827338ba5ae69083210d9aa7fdd998
Anna Jeon <Anna@Annas-MacBook-Air.local>
2020-06-23 21:56:01 -0400

    updated database with more employee inserted

--- seed.sql
+++ seed.sql
@@ -13,8 +13,8 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INTEGER,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    department_id INTEGER
+    -- FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -22,8 +22,8 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
-    manager_id INTEGER DEFAULT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    manager_id INTEGER DEFAULT NULL
+    -- FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
 SELECT * FROM department;
@@ -44,5 +44,9 @@ INSERT INTO role VALUES(0, "Accountant", 130000, 3);
 INSERT INTO role VALUES(0, "Legal Team Lead", 300000, 4);
 INSERT INTO role VALUES(0, "Lawyer", 195000, 4);
 
-INSERT INTO employee VALUES(0, "Anna", "Jeon", 3);
-
+INSERT INTO employee (id, first_name, last_name, role_id) VALUES(0, "Anna", "Jeon", 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES(0, "Two", "two", 2, 1);
+INSERT INTO employee (id, first_name, last_name, role_id) VALUES(0, "Alex", "Eum", 3);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES(0, "Four", "four", 4, 3);
+INSERT INTO employee (id, first_name, last_name, role_id) VALUES(0, "Brandon", "Jaggernauth", 5);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES(0, "Four", "four", 6, 5);
